d18bef9b44cc600a8d3e07b94659d280a19f820a
Lucas <lucas.martin.thomaz@gmail.com>
2024-10-05 20:07:43 +0200

    added player dash as well as wasd movement

--- player.gd
+++ player.gd
@@ -3,33 +3,40 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const dashTime = 0.3
+const DASHSPEED = 900
 
-
+var isDashing = false
+var xinput = 0
+var yinput = 0
+var xdir = 0
+var ydir = 0
 func _physics_process(_delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var xinput := Input.get_axis("ui_left", "ui_right")
-	var yinput := Input.get_axis("ui_up", "ui_down")
-
-	var dir = atan2(yinput, xinput)
-	var xdir = cos(dir)
-	var ydir = sin(dir)
-
-	if xinput:
-		velocity.x = xdir * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	if yinput:
-		velocity.y = ydir * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)	
+	updateMovement()
+	if Input.is_action_pressed("ui_dash"):
+		beginDash()
+	if !isDashing:
+		velocity.x = SPEED * xdir
+		velocity.y = SPEED * ydir
 	move_and_slide()
+
+func updateMovement():
+	if isDashing == false:
+		xinput = Input.get_axis("ui_left", "ui_right")
+		yinput = Input.get_axis("ui_up", "ui_down")
+		
+		if xinput != 0 and yinput != 0:
+			var dir = atan2(yinput, xinput)
+			xdir = cos(dir)
+			ydir = sin(dir)	
+		else:
+			xdir = xinput
+			ydir = yinput
+		
+
+func beginDash():
+	isDashing = true
+	velocity.x = DASHSPEED * xdir
+	velocity.y = DASHSPEED * ydir
+	await get_tree().create_timer(dashTime).timeout
+	isDashing = false
