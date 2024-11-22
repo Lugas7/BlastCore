062eebe68da768b270daf52c6ee211935ca8241b
Fredrik Grøttås <fredgrot@stud.ntnu.no>
2024-11-22 14:25:45 +0100

    Extend camera a little towards mouse

--- camera.gd
+++ camera.gd
@@ -1,5 +1,7 @@
 extends Camera2D
 
+const maxCameraExtend = 300.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +10,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	extendCameraTowardsMouse()
+
+
+func extendCameraTowardsMouse():
+	var mousePos = get_local_mouse_position()/4
+	
+	var left = -maxCameraExtend
+	var right = maxCameraExtend
+	var top = maxCameraExtend
+	var bottom = -maxCameraExtend
+	
+	var x = sign(mousePos.x)*min(abs(mousePos.x), maxCameraExtend)
+	var y = sign(mousePos.y)*min(abs(mousePos.y), maxCameraExtend)
+	position = Vector2(clamp(x, left, right), clamp(y, bottom, top))
